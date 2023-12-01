093376db56d577d82b69f63085066deb851d4ba3
Trevor LeBlanc <billbobphil@hotmail.com>
2023-11-30 19:24:50 -0800

    Fixed small issue with time resetting at end of game unintentionally. Added promotional screenshots for itch page

--- placeholder/EnableAbilitiesForTesting.gd
+++ placeholder/EnableAbilitiesForTesting.gd
@@ -3,9 +3,11 @@ extends Node2D
 @export var isDashEnabled : bool;
 @export var isScaleUpEnabled : bool;
 @export var isScaleDownEnabled : bool;
+@export var shouldResetStats : bool;
 
 func _ready():
 	AbilityFlags.isDashEnabled = isDashEnabled;
 	AbilityFlags.isScaleUpEnabled = isScaleUpEnabled;
 	AbilityFlags.isScaleDownEnabled = isScaleDownEnabled;
-	StatsTotals.reset();
+	if(shouldResetStats):
+		StatsTotals.reset();
